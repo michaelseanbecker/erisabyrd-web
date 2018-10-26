5c44884ae017128b0a08e8e0c9a64ea33cce59d5
Michael Sean Becker <geek@michaelseanbecker.com>
2018-10-25 22:26:07 -0700

    Updating header

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Readings with Erisa" />
     <Navbar />
     <div>{children}</div>
   </div>
